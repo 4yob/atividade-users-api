d4b6d8aaafb0e35c149788f0d045e8f47dfa0b61
Alejandra <alejandrabarros.tds1.senai@gmail.com>
2025-04-07 15:08:26 -0300

    refactor: adjust schema.sql

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -14,14 +14,14 @@ INSERT INTO users (name, email) VALUES
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     user_name VARCHAR(100) NOT NULL,
-    user_id INTEGER REFERENCES users(id) ON DELETE SET NULL
     title VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE SET NULL
 );
 
-INSERT INTO posts (title, content, user_id) VALUES
+INSERT INTO posts (user_name, title, content, user_id) VALUES
     ('Alejandra Barros', 'Back-End', 'Hoje eu farei o mínimo e esse será o meu máximo.', 1),
     ('Bruna Savelli', 'Passeio no parque', 'https://cdn.gazetasp.com.br/img/c/825/500/dn_arquivo/2022/05/caozinho.jpg', 2),
     ('Caio Gabriel', 'Salve', 'Suave na nave.', 3),
     ('Enzo Turcovic', 'Sono', 'Dormir em qlq lugar tem seus benefícios.', 4),
-    ('Kevin Lima', 'Nino', 'Kevinino', 5),
+    ('Kevin Lima', 'Nino', 'Kevinino', 5);
